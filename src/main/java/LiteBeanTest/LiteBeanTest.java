49d1439b14fb41d5667bf7a6ee00ec6557897025
krishna k maurya <krishna.maurya@paytmbank.com>
2022-08-10 20:22:18 +0530

    liteBeanTest with differnt class in order to CGLIB is applied here

--- src/main/java/LiteBeanTest/LiteBeanTest.java
+++ src/main/java/LiteBeanTest/LiteBeanTest.java
@@ -5,21 +5,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ComponentScan
 public class LiteBeanTest {
 
-    @Bean
-    public A getA(){
-        System.out.println("entering in a");
-        getB();
-        System.out.println("executing a");
-        A a = new A();
-        System.out.println(a);
-        System.out.println("again calling B");
-        getB();
-        return a;
-    }
+
 
     @Bean
     public B getB(){
