088ddeab3fa5b4f3584281a8ca2b610845c84fd4
krishna k maurya <krishna.maurya@paytmbank.com>
2022-08-15 10:39:27 +0530

    @ComponentScan

---
+++ src/main/java/differenceComponentScanVSAnnotationConfig/G_AnnotationOnly_With_ComponentScan/AppConfig.java
@@ -0,0 +1,13 @@
+package differenceComponentScanVSAnnotationConfig.G_AnnotationOnly_With_ComponentScan;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+
+ }
