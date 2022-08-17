0b66b3ecbb9ef1da6cf65d13442eacd33f45edc9
krishna k maurya <krishna.maurya@paytmbank.com>
2022-08-17 15:44:26 +0530

    Adding devtools dependency

---
+++ src/main/java/differenceComponentScanVSAnnotationConfig/J_spring_boot_With_Auto_reload/TestSpringBootApplication.java
@@ -0,0 +1,24 @@
+package differenceComponentScanVSAnnotationConfig.J_spring_boot_With_Auto_reload;
+
+import differenceComponentScanVSAnnotationConfig.I_spring_boot_impl_with_boot_dependency.innerPackage.A;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+
+@SpringBootApplication
+public class TestSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(TestSpringBootApplication.class);
+        String[] beanNames = context.getBeanDefinitionNames();
+        for( String beanName : beanNames){
+            System.out.println(beanName);
+        }
+    }
+
+}
---
+++ src/main/java/differenceComponentScanVSAnnotationConfig/J_spring_boot_With_Auto_reload/innerPackage/enums/Action.java
@@ -0,0 +1,8 @@
+package differenceComponentScanVSAnnotationConfig.J_spring_boot_With_Auto_reload.innerPackage.enums;
+
+public enum Action {
+    ADDITION,
+    SUBTRACTION,
+    MULTIPLICATION,
+    DIVISION
+}
